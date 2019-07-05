6ade1804878a3c611235c40f8cbd4f25666915af
jainal gosaliya <jainal09gosalia@gmail.com>
2019-07-05 18:47:13 +0530

    Delete test.py

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-import chardet
-encoding = chardet.detect('r�xirfg')['encoding'] 
-unicode_string = unicode('r�xirfg', encoding)
-my_ascii = unicode_string.encode('ascii', 'ignore')
-my_uni_string = my_ascii.decode('utf8')
-print(my_uni_string)
